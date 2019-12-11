c56fba2ea7858e5ad201a860b34f96aefaebd1f4
Sehrish Balouch <sehrishbalouch4@gmail.com>
2019-12-11 18:12:40 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,6 +1,6 @@
 speakers = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
 def badge_maker(name)
   speakers.each do |name|
-    puts "Hello, my name is #{name [count]}"
-    counter += 1 
+    puts "Hello, my name is #{name}"
+    
   end 
